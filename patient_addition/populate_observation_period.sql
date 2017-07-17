249c327f22d3dec4160244e7745a92679bed2e94
Samuel Rosko <samrosko25@gmail.com>
2017-07-17 15:48:05 -0400

    small change to support cohort definition

--- patient_addition/populate_observation_period.sql
+++ patient_addition/populate_observation_period.sql
@@ -1,3 +1,5 @@
 INSERT INTO observation_period (observation_period_id, person_id, observation_period_start_date, observation_period_end_date, period_type_concept_id)
 SELECT row_number() over(), person_id, visit_start_date, visit_end_date, 44814722
 FROM visit_occurrence 
+INSERT INTO observation_period (observation_period_id, person_id, observation_period_start_date, observation_period_end_date, period_type_concept_id) values (47526, 131, '2008-02-13', '2008-02-13', 44814722)
+INSERT INTO observation_period (observation_period_id, person_id, observation_period_start_date, observation_period_end_date, period_type_concept_id) values (47527, 131, '2008-03-12', '2008-03-12', 44814722)
