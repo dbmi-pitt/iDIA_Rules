79ea8bad60e83bd651b0c132a925240a5549e6bf
Richard D Boyce, PhD <boycer@u.washington.edu>
2018-01-02 12:33:29 -0700

    edited print line for Metoclopramide and an Antipsychotic or Cholinesterase inhibitor
    tested trigger
    
    
    Former-commit-id: 711aa6664464b99369117a1dd767a9b3c5963175

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -93,7 +93,8 @@ public class DroolsTest {
 	// String dateStr = "2016-02-16"; // Amiodarone - QT prolonging agents for banner population
 	// String dateStr = "2016-02-15"; // Fluconazole - Opioids for banner population
 	// String dateStr = "2016-02-06"; // Immunonosupressants - Azole antifungals for banner population
-	String dateStr = "2016-01-16"; // K - K-sparing diuretics for banner population 
+	// String dateStr = "2016-01-16"; // K - K-sparing diuretics for banner population 
+	String dateStr = "2016-03-29"; // Metoclopramide and an Antipsychotic or Cholinesterase inhibitor for banner population
 	
 	// Get concept ids and names from the defined concept sets. There is currently no hibernate mapping for this.
 	SQLQuery query = hibernateSession.createSQLQuery("SELECT concept_set_name,concept_id FROM ohdsi.concept_set cs INNER JOIN ohdsi.concept_set_item csi ON cs.concept_set_id = csi.concept_set_id");
