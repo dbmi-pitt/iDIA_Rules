88d2e78048f9dfcb1809cd5038f0e9c5c00b9fb0
Eric Chou <echou53@gmail.com>
2019-07-11 13:37:14 -0400

    total population epi bb numbers

--- patient_addition/sql/epi-bb-descriptive-sandbox.sql
+++ patient_addition/sql/epi-bb-descriptive-sandbox.sql
@@ -304,3 +304,56 @@ AND ((de1.drug_exposure_start_date <= o.observation_period_end_date AND de1.drug
 OR (de1.drug_exposure_end_date <= o.observation_period_end_date AND de1.drug_exposure_end_date >= o.observation_period_start_date)))
 ORDER BY de1.drug_exposure_start_datetime ASC
 ;
+
+--------------------------------------------------------------------------------------
+--epi & bb numbers across *all* patients 
+--------------------------------------------------------------------------------------
+
+SELECT de1.person_id, de1.drug_exposure_id AS epi_dexp, de1.drug_concept_id AS epi_id, c1.concept_name AS epi_name, cs1.concept_name AS epi_ingr, de1.drug_exposure_start_datetime AS epi_start, de1.drug_exposure_end_datetime AS epi_end,
+de2.drug_exposure_id AS bb_dexp, de2.drug_concept_id AS bb_id, c2.concept_name AS bb_name, cs2.concept_name AS bb_ingr, de2.drug_exposure_start_datetime AS bb_start, de2.drug_exposure_end_datetime AS bb_end, 
+o.observation_period_start_date AS obs_start, o.observation_period_end_date AS obs_end
+FROM drug_exposure de1 -- epinephrine
+INNER JOIN drug_exposure de2 ON de1.person_id = de2.person_id -- bb
+INNER JOIN observation_period o ON o.person_id = de1.person_id
+INNER JOIN concept c1 ON de1.drug_concept_id = c1.concept_id
+INNER JOIN concept c2 ON de2.drug_concept_id = c2.concept_id
+INNER JOIN drug_strength ds1 ON ds1.drug_concept_id = de1.drug_concept_id
+INNER JOIN concept cs1 ON ds1.ingredient_concept_id = cs1.concept_id
+INNER JOIN drug_strength ds2 ON ds2.drug_concept_id = de2.drug_concept_id
+INNER JOIN concept cs2 ON ds2.ingredient_concept_id = cs2.concept_id
+WHERE de1.drug_concept_id IN (select distinct concept_id from ohdsi.concept_set_item where concept_set_id = 7728)
+AND de2.drug_concept_id IN (select distinct concept_id from ohdsi.concept_set_item where concept_set_id = 7773)
+AND ds1.ingredient_concept_id IN (select distinct concept_id from ohdsi.concept_set_item where concept_set_id = 7584) -- epinephrines ingredients
+AND ds2.ingredient_concept_id IN (select distinct concept_id from ohdsi.concept_set_item where concept_set_id = 7756) -- beta-blockers ingredients
+AND (de2.drug_exposure_start_datetime < de1.drug_exposure_start_datetime AND de2.drug_exposure_end_datetime >= de1.drug_exposure_start_datetime)
+AND (de2.drug_exposure_start_date <= o.observation_period_end_date AND de2.drug_exposure_end_date >= o.observation_period_start_date)
+AND (('2016-01-01' BETWEEN o.observation_period_start_date AND o.observation_period_end_date)
+OR ('2016-04-30' BETWEEN o.observation_period_start_date AND o.observation_period_end_date))
+AND de1.drug_exposure_id != de2.drug_exposure_id
+ORDER BY de1.drug_exposure_start_datetime ASC;
+
+-- TODO count the above basic concomitant exposures for people with multiple exposures
+
+-- frequency of each bb across ALL patients
+SELECT c.concept_name AS bb_ingr, 
+COUNT(DISTINCT de.drug_exposure_id) AS freq_dexp,  
+COUNT(DISTINCT de.person_id) AS freq_person
+FROM drug_exposure de
+INNER JOIN concept c ON c.concept_id = ds.drug_concept_id
+INNER JOIN drug_strength ds ON ds.drug_concept_id = de.drug_concept_id
+WHERE ds.ingredient_concept_id IN (select distinct concept_id from ohdsi.concept_set_item where concept_set_id = 7756)
+GROUP BY c.concept_name
+ORDER BY freq_dexp DESC
+;
+
+-- frequency of all epi drugs
+SELECT c.concept_name AS epi_ingr, 
+COUNT(DISTINCT de.drug_exposure_id) AS freq_dexp,  
+COUNT(DISTINCT de.person_id) AS freq_person
+FROM drug_exposure de
+INNER JOIN concept c ON c.concept_id = ds.drug_concept_id
+INNER JOIN drug_strength ds ON ds.drug_concept_id = de.drug_concept_id
+WHERE de.drug_concept_id IN (select distinct concept_id from ohdsi.concept_set_item where concept_set_id = 7728)
+GROUP BY c.concept_name
+ORDER BY freq_dexp DESC
+;
