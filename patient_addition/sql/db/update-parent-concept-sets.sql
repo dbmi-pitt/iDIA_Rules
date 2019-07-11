13dcf8cc48fbb83c73a3c7be16ae81febf3a0bc9
Eric <eeeric.chou@gmail.com>
2019-07-11 15:17:50 -0400

    script for updating 'History of GI Bleeds' condition concept set
    Former-commit-id: b2e8586a56c9d167fcd9604a609a2e37ae09ccd4

--- patient_addition/sql/db/update-parent-concept-sets.sql
+++ patient_addition/sql/db/update-parent-concept-sets.sql
@@ -121,3 +121,34 @@ INSERT INTO ohdsi.concept_set_item (concept_set_id, concept_id, is_excluded, inc
 DROP TABLE ohdsi.temp_concept_set_item;
 
 COMMIT TRANSACTION;
+
+/*
+Update "History of GI Bleeds" concept set based on ICD9/10 codes in protocol
+*/
+
+BEGIN TRANSACTION;
+
+CREATE TABLE ohdsi.temp_concept_set_item (
+  concept_set_item_id serial NOT NULL,
+  concept_set_id int4 NOT NULL,
+  concept_id int4 NOT NULL,
+  is_excluded int4 NOT NULL,
+  include_descendants int4 NOT NULL,
+  include_mapped int4 NOT NULL
+);
+
+INSERT INTO ohdsi.temp_concept_set_item
+(select i.concept_set_item_id, i.concept_set_id, i.concept_id, i.is_excluded, i.include_descendants, i.include_mapped 
+from ohdsi.concept_set cs
+inner join ohdsi.concept_set_item i
+on i.concept_set_id = cs.concept_set_id
+where cs.concept_set_name in ('History of GI Bleeds'));
+
+DELETE FROM ohdsi.concept_set_item WHERE concept_set_id = 6094;
+
+INSERT INTO ohdsi.concept_set_item (concept_id, concept_set_id, is_excluded, include_descendants, include_mapped)
+(select distinct concept_id, 6094 as concept_set_id, 0 as is_excluded, 0 as include_descendants, 0 as include_mapped from concept where concept_code in ('578.0','532.00','578.9','530.21','530.82','531.0','531.2','531.4','531.6','532.0','532.2','532.4','532.6','533.0','533.2','533.4','533.6','534.0','534.2','534.4','534.6','K92.2','K25.0','K25.2','K25.4','K25.6','K26.0','K26.2','K26.4','K26.6','K27.0','K27.2','K27.4','K27.6','K28.0','K28.2','K28.4','K28.6','K29.01','K29.31','K29.41','K29.51','K29.61','K29.71','K29.81','K29.91','K31.811','K31.82'));
+
+DROP TABLE ohdsi.temp_concept_set_item;
+
+COMMIT TRANSACTION;
