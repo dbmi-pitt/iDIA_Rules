8fd5782710f8d7caec57d498bded333eea28f96d
mas400 <mas400@mas400-mdt.dbmi.pitt.edu>
2019-11-11 11:06:37 -0500

    Adding new script file to be ran by docker container
    Former-commit-id: 1d0fc36928d7b5b4e710d3d6aee1983bcf3bee6b

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-/etc/init.d/postgresql start
-sleep 10
+service postgresql start
+RETRIES=5
 
+until psql -U                     # replace with the empty string -d                     # replace with the empty string -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 5
+done
 echo "Arguments: $@"
 ./runRules.sh $@
