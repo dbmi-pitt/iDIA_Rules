6444bd1c69a73408fd2ebb1c20d479ed16a28ef5
mas400 <mas400@mas400-mdt.dbmi.pitt.edu>
2019-11-11 11:19:06 -0500

    Adding new script file to be ran by docker container

--- dockerRun.sh
+++ dockerRun.sh
@@ -3,9 +3,9 @@
 service postgresql start
 RETRIES=5
 
-until psql -U idiarules -d idiarules -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+until psql -h 127.0.0.1 -U idiarules -d idiarules -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
-  sleep 5
+  sleep 10
 done
 echo "Arguments: $@"
 ./runRules.sh $@
